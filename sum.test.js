a5835702b5c3d5c190ca1b0873914e6d40aa3b32
nepalrohan <rohannepal098@gmail.com>
2025-02-12 22:02:07 +0545

    matchers:toEqual()

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,21 @@
-const sum = require('./sum');
+// const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+
+
+
+// test('two plus two is four' ,()=>{
+//   expect(2+2).toBe(4);
+// })
+
+
+
+test('object assigment', ()=>{
+const data = { one:1};
+data['two'] =2;
+
+expect(data).toEqual({one:1, two:2});
+})
